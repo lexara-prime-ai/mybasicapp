426fd6ccc206de25f1ab5155a34896e0271a5110
irfanghat <irfanghat@gmail.com>
2023-06-08 15:47:17 +0300

    NgRx Effects with Selectors and Entities

--- src/app/components/customers/state/customer.reducer.ts
+++ src/app/components/customers/state/customer.reducer.ts
@@ -2,12 +2,14 @@ import * as customerActions from './customer.actions';
 
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
+
 import { Customer } from '../customer.model';
 import * as fromRoot from '../../../state/app.state';
-import { state } from '@angular/animations';
 
-export interface CustomerState {
-    customers: Customer[],
+
+export interface CustomerState extends EntityState<Customer> {
+    selectedCustomerId: number | null,
     loading: boolean,
     loaded: boolean,
     error: string
@@ -17,12 +19,18 @@ export interface AppState extends fromRoot.AppState {
     customers: CustomerState,
 }
 
-export const initialState: CustomerState = {
-    customers: [],
+export const customerAdapter: EntityAdapter<Customer> = createEntityAdapter<Customer>();
+
+export const defaultCustomer: CustomerState = {
+    ids: [],
+    entities: {},
+    selectedCustomerId: null,
     loading: false,
     loaded: false,
     error: ""
 }
+
+export const initialState = customerAdapter.getInitialState(defaultCustomer);
 
 export function customerReducer(
     state = initialState,
@@ -36,17 +44,16 @@ export function customerReducer(
             }
         }
         case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_SUCCESS: {
-            return {
-                    ...state,
+            return customerAdapter.addMany(action.payload, {
+                ...state,
                 loading: false,
-                loaded: true,
-                customers: action.payload
-            }
+                loaded: true
+            })
         }
         case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_FAIL: {
             return {
                 ...state,
-                customers: [],
+                entities: {},
                 loading: false,
                 loaded: false,
                 error: action.payload
@@ -64,7 +71,7 @@ const getCustomerFeatureState = createFeatureSelector<CustomerState>(
 )
 
 export const getCustomers = createSelector(getCustomerFeatureState,
-    (state: CustomerState) => state.customers
+    customerAdapter.getSelectors().selectAll
 )
 
 export const getCustomersLoading = createSelector(getCustomerFeatureState,
